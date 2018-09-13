20b959f180ac16fd85f8758cb2cebc8dc2fe62dd
Shawn Hassen <Shassen89@gmail.com>
2018-09-13 15:31:40 -0400

    replaced ul tags with divs

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ class App extends Component {
         <h1>Shawn Hassen</h1>
         <p>Welcome to my portfolio!</p>
         <p>Click the links below to checkout projects i'm working on:</p>
-        <ul>
+        <div>
           <a href="https://shassen.github.io/reaction-arrow-project1/">Reaction Arrow Game</a>
           <a href="https://floating-waters-77222.herokuapp.com/auth/login">News Crews App</a>
           <a href="http://earthy-lunch.surge.sh/">People Are People App</a>
           <a href="http://voracious-sticks.surge.sh/">Divy App</a>
-        </ul>
+        </div>
       </div>
     );
   }
