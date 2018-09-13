61566c4404e49fd0894745904fd4ad719b029823
Shawn Hassen <Shassen89@gmail.com>
2018-09-13 14:44:07 -0400

    Initialized repo, created react app and deployed to github pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Shawn Hassen</h1>
+        <ul>
+          <a href="https://shassen.github.io/reaction-arrow-project1/">Reaction Arrow Game</a>
+          <a href="https://floating-waters-77222.herokuapp.com/auth/login">News Crews App</a>
+          <a href="http://earthy-lunch.surge.sh/">People Are People App</a>
+          <a href="http://voracious-sticks.surge.sh/">Divy App</a>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
