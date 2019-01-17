987ce2d9221ae661a5aed1d0ff60bacaa0c09175
Shawn Hassen <Shassen89@gmail.com>
2019-01-17 17:06:27 -0500

    Updated bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,8 +8,36 @@ export default function About(props) {
       <h1 className="headers"><strong>About Me:</strong></h1><br />
       <div className="profile">
       <img className="profile-img" src="https://user-images.githubusercontent.com/34017019/51342654-6f6c5a80-1a62-11e9-906a-655c4d4bc396.jpg" />
-      <p className="profile-bio">I am a proactive web developer committed to the pursuit of debugged applications through concise, clean and intentional code. I have experience working with both front and back-end techniques, and enjoy both as they present unique challenges to solve. 
-         My experience in finance has created a passion for working with data and developing solutions for automating mundane workflows. My favorite aspect of web development is that I have the ability to mold the future of technology and build a better world. Let’s build a better future together.
+      <p className="profile-bio">
+        Upon graduating from Kent State University in Ohio with a Bachelors degree in Finance, I quickly realized
+        my passions didn't include calculating ROIs or betas but rather fitness, music and
+        technology.
+        <br />
+        <br />
+        Crunch Fitness HQ in NYC is where I
+        established myself as an accountant, tracking cash for the fitness behemoth. My colleauges were great and the
+        atmosphere was energetic, but something was missing - passion. Simply placing myself in the fitness industry
+        didn't change the fact that I was eager for more than what accounting could offer.
+        <br />
+        <br />
+        My next move was a lateral one, but still exciting because I transitioned out of the fitness industry and into technology.
+        Learnvest, also located in NYC, was a start-up created for millenials to track their finances in the digital space.
+        Without even realizing it, I took up a strong interest in programming because I was surrounded by software engineers.
+        I would go home after work and learn as much as a I could about coding principals. The software engineers at Learnvest
+        suggested I learn Javascript, so I did! For six months I practiced coding every night and quickly fell in love with the craft.
+        <br />
+        <br />
+        Enter General Assembly. I decided to take a leap of faith and enrolled in a web development bootcamp at GA, a 12-week 
+        immersive program designed to teach students the skills to thrive as a software developer. Here, I learned how to 
+        effectively use HTML5, CSS3, Javascript, Node, Express, React, PostgreSQL, Ruby on Rails and design principals to build
+        creative web pages and apps. 
+        <br />
+        <br />
+        If you've gotten this far, I thank you for reading and visiting my site! Feel free to browse, look at my projects, connect with me and 
+        provide any constructive feedback you may have.
+        <br />
+        <br />
+        Shawn H.
       </p>
       </div>
     </div>
