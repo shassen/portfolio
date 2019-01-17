989507943c350c59580aa4a07e572a4f8ea2ecda
Shawn Hassen <Shassen89@gmail.com>
2019-01-17 14:17:15 -0500

    Fixed link to photo in aboutme section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About(props) {
     <div className="container">
       <h1 className="headers"><strong>About Me:</strong></h1><br />
       <div className="profile">
-      <img className="profile-img" src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/11140079_10153288818007188_4872141858572481998_n.jpg?_nc_cat=107&oh=62d5b724ad02c18cbdc91f8694a60d2f&oe=5C353768" />
+      <img className="profile-img" src="https://user-images.githubusercontent.com/34017019/51342654-6f6c5a80-1a62-11e9-906a-655c4d4bc396.jpg" />
       <p className="profile-bio">I am a proactive web developer committed to the pursuit of debugged applications through concise, clean and intentional code. I have experience working with both front and back-end techniques, and enjoy both as they present unique challenges to solve. 
          My experience in finance has created a passion for working with data and developing solutions for automating mundane workflows. My favorite aspect of web development is that I have the ability to mold the future of technology and build a better world. Let’s build a better future together.
       </p>
